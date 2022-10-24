4e80f024edec7d27db381b0780c52d47039a8526
hngmnj <20203104@gs.cwnu.ac.kr>
2022-10-24 10:04:05 +0900

    ctgry -> category

--- model/tables.sql
+++ model/tables.sql
@@ -86,7 +86,7 @@ create table stock(
 create table board(
 	brd_seq integer auto_increment primary key,
     brd_writer varchar(20) not null,
-	ctgry varchar(20) not null,
+	category varchar(20) not null,
     title varchar(40) not null,
     brd_content varchar(400) not null,
     brd_date varchar(20) default (curdate()) not null,
