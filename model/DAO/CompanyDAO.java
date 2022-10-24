ffcf0d68d512a6748a4a57ff3791d7f20871fecd
hngmnj <20203104@gs.cwnu.ac.kr>
2022-10-24 13:14:34 +0900

    Company DAO

---
+++ model/DAO/CompanyDAO.java
@@ -0,0 +1,69 @@
+package gntp.bbulsora.project.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import gntp.bbulsora.project.vo.CompanyVO;
+
+@Repository("CompanyDAO")
+public class CompanyDAO {
+
+	@Autowired
+	private SqlSession sqlSession;
+	public CompanyDAO() {}
+
+
+	public List<CompanyVO> selectAll() throws SQLException {
+		List<CompanyVO> list = null;
+		list = sqlSession.selectList("mapper.company.selectAllCompanyList");
+		return list;
+	}
+
+	public List<CompanyVO> selectClient() {
+		List<CompanyVO> list = null;
+		list = sqlSession.selectList("mapper.company.selectClientList");
+		return list;
+	}
+
+	public List<CompanyVO> selectSupply() {
+		List<CompanyVO> list = null;
+		list = sqlSession.selectList("mapper.company.selectSupplyList");
+		return list;
+	}
+
+	public CompanyVO selectOne(String compCd) throws SQLException {
+		CompanyVO company = null;
+		company = sqlSession.selectOne("mapper.company.selectCompanyByCompCd", compCd); 
+		return company;
+	}
+
+	public boolean insertOne(CompanyVO company) throws SQLException {
+		boolean flag = false;
+		int affectedCount = sqlSession.insert("mapper.company.insertCompany", company);
+		if(affectedCount > 0)
+			flag = true;
+		System.out.println("Insert company: "+flag);
+		return flag;
+	}
+
+	public boolean updateOne(CompanyVO company) throws SQLException {
+		boolean flag = false;
+		int affectedCount = sqlSession.update("mapper.company.updateCompany", company);
+		if(affectedCount > 0)
+			flag = true;
+		return flag;
+	}
+
+	public boolean deleteOne(CompanyVO company) throws SQLException {
+		boolean flag = false;
+		int affectedCount = sqlSession.delete("mapper.company.deleteCompany", company);
+		if(affectedCount > 0)
+			flag = true;
+		return flag;
+	}
+
+}
