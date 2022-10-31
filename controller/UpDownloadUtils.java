fe808798293439b17e1b026e0074ad4d28212e38
JeongKM8 <110527169+JeongKM8@users.noreply.github.com>
2022-10-31 15:42:56 +0900

    다운로드 기능 분리

---
+++ controller/UpDownloadUtils.java
@@ -0,0 +1,38 @@
+package gntp.bbulsora.project.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UpDownloadUtils {
+	public static void download(String fileName, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String browser = request.getHeader("User-Agent");
+		if(browser.contains("MSIE")||browser.contains("Trident")||browser.contains("Chrome")) {
+			fileName = URLEncoder.encode(fileName, "UTF-8").replaceAll("\\+", "%20");
+		} else {
+			fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		}
+		String realFileName = "D:\\test\\"+fileName;
+		System.out.println(realFileName);
+		File file1 = new File(realFileName);
+		if(!file1.exists()) {
+			return ;
+		} 
+		response.setContentType("application/octer-stream");
+		response.setHeader("Content-Transfer-Encoding", "binary;");
+		response.setHeader("content-Disposition", "attachment; fileName=\""+fileName+"\"");
+		OutputStream os = response.getOutputStream();
+		FileInputStream fis = new FileInputStream(realFileName);
+		int ncount = 0;
+		byte[] bytes = new byte[512];
+		while((ncount = fis.read(bytes)) != -1) {
+			os.write(bytes, 0, ncount);
+		}
+		fis.close();
+		os.close();
+	}
+}
