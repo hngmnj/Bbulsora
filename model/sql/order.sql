7ddf10f167fa5507544415c8fd23bee4e0fc5ad1
chlgudwns12 <110008744+chlgudwns12@users.noreply.github.com>
2022-11-01 14:10:48 +0900

    Add files via upload

--- model/sql/order.sql
+++ model/sql/order.sql
@@ -1,11 +1,11 @@
+use bbsr;
+
 create table order_t(
     order_seq integer auto_increment primary key,
 	order_cd varchar(20) not null,
     order_qtt integer not null,
-    submit_date varchar(20) not null,
-    order_date varchar(20) not null,
-    from_date varchar(20),
-    to_date varchar(20),
+    submit_date varchar(20) not null default (curdate()),
+    order_date varchar(20),
     comp_cd varchar(20) not null,
     foreign key (comp_cd) references company(comp_cd) on delete cascade,
     item_cd varchar(20) not null,
