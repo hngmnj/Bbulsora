2aa0ceba0220c7d0297f47ccbbaca87a27bf4cea
hngmnj <20203104@gs.cwnu.ac.kr>
2022-12-02 13:52:26 +0900

    Delete model directory

--- model/sql/comment.sql
+++
@@ -1,8 +0,0 @@
-create table comment(
-	cmnt_seq integer auto_increment primary key,
-	cmnt_writer varchar(20) not null,
-    cmnt_content varchar(400) not null,
-	cmnt_date varchar(20) default (curdate()) not null,
-	brd_seq integer not null,
-	foreign key (brd_seq) references board(brd_seq) on delete cascade
-);
--- model/sql/state.sql
+++
@@ -1,4 +0,0 @@
-create table state(
-	state_cd varchar(20) primary key,
-    state_content varchar(20) not null
-);
