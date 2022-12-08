4abef4e1e65addfe88c6528a8c5338c11b5bd4d5
JeongKM8 <power9402@naver.com>
2022-12-08 13:48:39 +0900

    filepath update

--- Bbulsora_1128/src/main/java/gntp/bbulsora/project/utils/Filepaths.java
+++ Bbulsora_1128/src/main/java/gntp/bbulsora/project/utils/Filepaths.java
@@ -3,7 +3,7 @@ package gntp.bbulsora.project.utils;
 public class Filepaths {
 
 	public static final String UP_DOWN_PATH = "/home/ubuntu/dev/downloads/";
-	public static final String IMG_PATH = "/home/ubuntu/dev/img/";
+	public static final String IMG_PATH = "/home/ubuntu/dev/images/";
 	
 	public static final String LOCAL_UP_DOWN_PATH = "D:\\dev\\test\\downloads\\";
 	public static final String LOCAL_IMG_PATH = "D:\\dev\\test\\img\\";
